f1cf333b6dc4794d860386ae089efd9217b17c26
leothorp <leo.thorp1@gmail.com>
2015-10-12 19:21:10 -0500

    t

--- server/config/config.js
+++ server/config/config.js
@@ -6,6 +6,8 @@ catch (err) {}
 module.exports = {
   port: 8080,
   localdb: 'mongodb://127.0.0.1:27017/habitdb',
-  tokenSecret: process.env.tokenSecret || secret.tokenSecret,
-  googleSecret: process.env.googleSecret || secret.googleSecret
+  // tokenSecret: process.env.tokenSecret || secret.tokenSecret,
+  // googleSecret: process.env.googleSecret || secret.googleSecret
+  tokenSecret: 'token',
+  googleSecret: 'google'
 };
