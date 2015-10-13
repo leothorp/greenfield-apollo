19cb2385b0794cbf068636d6ed3b421d87bf7905
leothorp <leo.thorp1@gmail.com>
2015-10-12 19:27:29 -0500

    token

--- server/config/config.js
+++ server/config/config.js
@@ -8,6 +8,6 @@ module.exports = {
   localdb: 'mongodb://127.0.0.1:27017/habitdb',
   // tokenSecret: process.env.tokenSecret || secret.tokenSecret,
   // googleSecret: process.env.googleSecret || secret.googleSecret
-  tokenSecret: 'token',
-  googleSecret: 'google'
+  tokenSecret: 'yoursecretkeyhere',
+  googleSecret: 'fHIrD60a6FSjk4IA58EKBZh2'
 };
